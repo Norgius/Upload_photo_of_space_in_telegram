7b0dbfa7d0013a08444b6f1db9a2bcffa705c7ba
Norgius <dmitry.schadrin21@yandex.ru>
2022-09-22 22:37:26 +0300

    Publish photo to Telegram channel.

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,10 +1,15 @@
+from telegram import InputMediaPhoto
 import telegram
 import os
 
 from dotenv import load_dotenv
+from pathlib import Path
 
 
 load_dotenv()
 
 bot = telegram.Bot(token=os.getenv('TELEGRAM_SPACE_BOT'))
-bot.send_message(text='Hi John!', chat_id='@space_photos_prime')
+
+media_1 = InputMediaPhoto(media=open(Path("images/nasa_apod_0.jpg"), "rb"))
+bot.send_media_group(chat_id='@space_photos_prime',
+                     media=[media_1])
