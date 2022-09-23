20590b78947f368d76761496236d7f6c494416e4
Norgius <dmitry.schadrin21@yandex.ru>
2022-09-23 22:20:25 +0300

    Splited the function

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,39 +1,64 @@
-from random import shuffle
-from telegram import InputMediaPhoto
-import telegram
+from random import shuffle, choice
+from pathlib import Path
 import argparse
 import time
 import os
 
+import telegram
+from telegram import InputMediaPhoto
 from dotenv import load_dotenv
-from pathlib import Path
 
 
 load_dotenv()
 Path('images/').mkdir(parents=True, exist_ok=True)
 
+
+def check_files():
+    files = list(os.walk(Path("images/")))[0][2]
+    if not files:
+        raise TypeError("Изображения отсутствуют, пожалуйста загрузите их")
+    return files
+
+
+def post_one_photo(filename=''):
+    files = check_files()
+    if not filename:
+        filename = choice(files)
+    if filename not in files:
+        print("Данный файл отсутствует в images/")
+        return
+    photo = InputMediaPhoto(media=open(Path(f"images/{filename}"), "rb"))
+    bot.send_media_group(chat_id='@space_photos_prime',
+                         media=[photo])
+
+
+def post_endlessly():
+    while True:
+        files = check_files()
+        shuffle(files)
+        for filename in files:
+            photo = InputMediaPhoto(media=open(
+                        Path(f"images/{filename}"), "rb"))
+            bot.send_media_group(chat_id='@space_photos_prime',
+                                 media=[photo])
+            time.sleep(14400)
+
+
 parser = argparse.ArgumentParser(
-    description='''Интервал публикации изображений в \
-                   телеграм канале, по умолчанию - 4 часа'''
+    description='''Публикует в телеграм канале отдельное фото, \
+                   случайное фото или публикует фотографии в \
+                   бесконечном цикле с интервалом 4 часа'''
 )
-parser.add_argument('-t', '--time', default=None, type=int,
-                    help='Частота публикаций')
+parser.add_argument('-p', '--photo', default=None, type=str,
+                    help='''-p cycle - бесконечный цикл;
+                         -p [filename] - публикация фото filename;
+                         без аргументов - публикация случайного фото'''
+                    )
 args = parser.parse_args()
-if args.time:
-    interval = args.time * 3600
-else:
-    interval = 14400
-
 bot = telegram.Bot(token=os.getenv('TELEGRAM_SPACE_BOT'))
-
-while True:
-    images = list(os.walk(Path("images/")))[0][2]
-    if not images:
-        print("Изображения отсутствуют, пожалуйста загрузите их")
-        break
-    shuffle(images)
-    for image in images:
-        photo = InputMediaPhoto(media=open(Path(f"images/{image}"), "rb"))
-        bot.send_media_group(chat_id='@space_photos_prime',
-                             media=[photo])
-        time.sleep(interval)
+if args.photo == 'cycle':
+    post_endlessly()
+elif args.photo:
+    post_one_photo(args.photo)
+else:
+    post_one_photo()
