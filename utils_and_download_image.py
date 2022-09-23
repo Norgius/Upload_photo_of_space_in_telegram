2b5848c1af219acd4cda789d4ac39367408e966c
Norgius <dmitry.schadrin21@yandex.ru>
2022-09-23 19:54:35 +0300

    Remained utilities and download func

---
+++ utils_and_download_image.py
@@ -0,0 +1,29 @@
+import os
+from urllib.parse import urlsplit
+from pathlib import Path
+
+import requests
+from dotenv import load_dotenv
+
+
+Path('images/').mkdir(parents=True, exist_ok=True)
+load_dotenv()
+
+headers = {
+    'User-Agent': 'My User Agent 1.0'
+}
+
+
+def get_file_extension(link):
+    parsed_link = urlsplit(link)
+    filename = os.path.split(parsed_link.path)[1]
+    return os.path.splitext(filename)[1]
+
+
+def download_image(url, path, params=None):
+    if params is None:
+        params = {}
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+    with open(path, "wb") as file:
+        file.write(response.content)
