af58aaaadabefc1c1ebf4f183afccc897b675211
Norgius <dmitry.schadrin21@yandex.ru>
2022-09-27 21:33:01 +0300

    Deleted spacex_id variable

--- fetch_spacex_last_launch.py
+++ fetch_spacex_last_launch.py
@@ -5,9 +5,6 @@ import os
 import requests
 
 
-spacex_id = '5eb87d47ffd86e000604b38a'
-
-
 def fetch_spacex_last_launch(id):
     url_spaceX = f'https://api.spacexdata.com/v5/launches/{id}'
     response = requests.get(url_spaceX, headers=headers)
