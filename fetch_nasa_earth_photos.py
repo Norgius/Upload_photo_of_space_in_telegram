c2efb4828eed6adac0994fab615205d73f424905
Norgius <dmitry.schadrin21@yandex.ru>
2022-09-27 21:31:03 +0300

    Added argparse() and splited the main function

--- fetch_nasa_earth_photos.py
+++ fetch_nasa_earth_photos.py
@@ -1,21 +1,19 @@
 from utils_and_download_image import headers, download_image
 import datetime
+import argparse
 import os
 
 import requests
 from dotenv import load_dotenv
 
 
-def main():
-    load_dotenv()
-    api_key = os.getenv('NASA_KEY')
+def fetch_nasa_earth_photos(api_key, number):
     url_earth = 'https://api.nasa.gov/EPIC/api/natural/images'
     params = {'api_key': api_key}
     response = requests.get(url_earth, headers=headers, params=params)
     response.raise_for_status()
-
     for counter, response_part in enumerate(response.json()):
-        if counter == 7:
+        if counter == number:
             break
         date = response_part.get('date')
         date_for_url = datetime.datetime.fromisoformat(
@@ -25,6 +23,20 @@ def main():
             'natural/{}/png/{}.png'.format(date_for_url, filename)
         path = os.path.join('images', f'nasa_earth_{counter}.png')
         download_image(nasa_earth_link, path, params)
+
+
+def main():
+    load_dotenv()
+    api_key = os.getenv('NASA_KEY')
+    parser = argparse.ArgumentParser(
+        description='Данная программа позволяет '
+                    'скачивать фотографии нашей планеты'
+    )
+    parser.add_argument('-n', default='7', type=int,
+                        help='''Число для загрузки нужного \
+                                количества фотографий (от 1 до 10)''')
+    args = parser.parse_args()
+    fetch_nasa_earth_photos(api_key, args.n)
 
 
 if __name__ == '__main__':
